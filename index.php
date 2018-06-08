345f09cb5b703835af2e6012946ed2be445b32a6
unknown <issamaal2012@hotmail.com>
2018-06-08 15:17:57 +0700

    ADD

--- index.php
+++ index.php
@@ -56,8 +56,9 @@
     } 
     else if($message == "test"){
         $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
-        $arrayPostData['messages'][0]['type'] = "text";
-        $arrayPostData['messages'][0]['text'] = "อย่าทิ้งกันไป";
+        $arrayPostData['messages'][0]['type'] = "template";
+        $arrayPostData['messages'][0]['template']['type'] = "buttons";
+        $arrayPostData['messages'][0]['template']['thumbnailImageUrl'] = "https://www.nectec.or.th/articleImage/446";
         $arrayPostData['messages'][1]['type'] = "sticker";
         $arrayPostData['messages'][1]['packageId'] = "1";
         $arrayPostData['messages'][1]['stickerId'] = "131";
