ebc6eca7ba84ca6bfd790a05fd7603375d1c719a
unknown <issamaal2012@hotmail.com>
2018-06-08 15:13:57 +0700

    test

--- index.php
+++ index.php
@@ -57,37 +57,8 @@
     else if($message == "test"){
         $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
         $arrayPostData['messages'][0]['type'] = "template";
-        $arrayPostData['messages'][0]['template'] = '{
-            "type": "buttons",
-            "thumbnailImageUrl": "https://example.com/bot/images/image.jpg",
-            "imageAspectRatio": "rectangle",
-            "imageSize": "cover",
-            "imageBackgroundColor": "#FFFFFF",
-            "title": "Menu",
-            "text": "Please select",
-            "defaultAction": {
-                "type": "uri",
-                "label": "View detail",
-                "uri": "http://example.com/page/123"
-            },
-            "actions": [
-                {
-                  "type": "postback",
-                  "label": "Buy",
-                  "data": "action=buy&itemid=123"
-                },
-                {
-                  "type": "postback",
-                  "label": "Add to cart",
-                  "data": "action=add&itemid=123"
-                },
-                {
-                  "type": "uri",
-                  "label": "View detail",
-                  "uri": "http://example.com/page/123"
-                }
-            ]
-        }';
+        $arrayPostData['messages'][0]['template']['type'] = "buttons";
+        $arrayPostData['messages'][0]['template']['thumbnailImageUrl'] = "https://www.nectec.or.th/articleImage/446";
         replyMsg($arrayHeader,$arrayPostData);
     } 
 function replyMsg($arrayHeader,$arrayPostData){
